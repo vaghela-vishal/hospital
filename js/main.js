06c0b4b121836fc98f51bcea3a29ff0a97aa126e
vaghela-vishal <140404025+vaghela-vishal@users.noreply.github.com>
2023-08-08 22:32:45 +0530

    hospital
    hospital

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+
+  
+    
+     // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('.navbar').addClass('sticky-top');
+        } else {
+            $('.navbar').removeClass('sticky-top');
+        }
+    });
+    
+    
+    
+    // Price carousel
+    $(".price-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 45,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+
+  
+    
+
